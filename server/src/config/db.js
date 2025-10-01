149d6678324fe4f47230d784061a1345304a48c8
adhamashraf9 <adham4e5@gmail.com>
2025-10-01 10:00:59 +0300

    Update db.js

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 export async function connectDB() {
-  const uri = process.env.MONGO_URI;
+  const uri =  "mongodb://Hadwa:Hadwa@ac-tl77fwg-shard-00-00.hpstsct.mongodb.net:27017,ac-tl77fwg-shard-00-01.hpstsct.mongodb.net:27017,ac-tl77fwg-shard-00-02.hpstsct.mongodb.net:27017/?ssl=true&replicaSet=atlas-4jfj3v-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0I";
   if (!uri) throw new Error('MONGO_URI is not set');
   try {
     await mongoose.connect(uri, { autoIndex: true });
